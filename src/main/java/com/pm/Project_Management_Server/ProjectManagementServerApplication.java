addb892387086f226a1273640961da99714d4fc4
Abhik Chakraborty <abhik.chakraborty@bounteous.com>
2025-06-26 14:35:15 +0530

    first commit

---
+++ src/main/java/com/pm/Project_Management_Server/ProjectManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.pm.Project_Management_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pm/Project_Management_Server/ProjectManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.Project_Management_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
