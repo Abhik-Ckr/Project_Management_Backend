d55f749a0c85a4f18c58bed4bc41d2d7012e2222
Abhik Chakraborty <abhik.chakraborty@bounteous.com>
2025-06-27 16:45:35 +0530

    added some entities such as client, contactPerson, User and UserType(ENUM)

---
+++ src/main/java/com/pm/Project_Management_Server/entity/Client.java
@@ -0,0 +1,23 @@
+package com.pm.Project_Management_Server.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+import java.time.LocalDate;
+
+@Entity
+public class Client {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private LocalDate onBoardedOn;
+    @Min(0)
+    @Max(10)
+    private Integer clientRating;
+}
---
+++ src/main/java/com/pm/Project_Management_Server/entity/UserType.java
@@ -0,0 +1,6 @@
+package com.pm.Project_Management_Server.entity;
+
+public enum UserType {
+    ADMIN,
+    USER
+}
