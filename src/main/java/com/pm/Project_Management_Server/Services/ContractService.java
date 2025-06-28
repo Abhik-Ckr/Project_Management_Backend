8ee19dc6ec59a99aa769e0c435baacdffb894c58
Abhik Chakraborty <abhik.chakraborty@bounteous.com>
2025-06-29 00:13:47 +0530

    added and implemented Contract Entity, DTO, repo, sevice and controller

---
+++ src/main/java/com/pm/Project_Management_Server/Services/ContractService.java
@@ -0,0 +1,14 @@
+package com.pm.Project_Management_Server.Services;
+
+import com.pm.Project_Management_Server.dto.ContractDTO;
+import com.pm.Project_Management_Server.dto.ContractResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ContractService {
+    ContractResponseDTO createContract(ContractDTO dto);
+    List<ContractResponseDTO> getContractsByProject(Long projectId);
+    ContractResponseDTO updateContractAmount(Long contractId, Double newAmount);
+}
