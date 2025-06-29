c540cd5829eb32801b4b28f00cd41c84f3ad5106
Uppalapati Syam Karni Datta <uppalapati.datta@bounteous.com>
2025-06-29 15:47:44 +0530

    added contact person repository

---
+++ src/main/java/com/pm/Project_Management_Server/Repositories/ContactPersonRepository.java
@@ -0,0 +1,15 @@
+package com.pm.Project_Management_Server.Repositories;
+
+import com.pm.Project_Management_Server.entity.ContactPerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ContactPersonRepository extends JpaRepository<ContactPerson, Long> {
+    List<ContactPerson> findByName(String name);
+    Optional<ContactPerson> findByEmail(String email);
+
+}
