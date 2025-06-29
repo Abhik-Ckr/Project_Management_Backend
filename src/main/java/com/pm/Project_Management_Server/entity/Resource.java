5db77c65b0e32a6efcfeaa3f6bf3fa12ecd0818d
Uppalapati Syam Karni Datta <uppalapati.datta@bounteous.com>
2025-06-29 19:55:29 +0530

    migrated for lombok issues

--- src/main/java/com/pm/Project_Management_Server/entity/Resource.java
+++ src/main/java/com/pm/Project_Management_Server/entity/Resource.java
@@ -1,12 +1,16 @@
 package com.pm.Project_Management_Server.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Resource {
 
     @Id
@@ -29,10 +33,11 @@ public class Resource {
     private LocalDate endDate;
 
 
-
-    private int exp;
-
-    private boolean isAllocated;
+// migrated for lombok issues
+//    private int exp;
+    private Integer exp;
+//    private boolean isAllocated;
+    private boolean allocated;
 
 
     }
