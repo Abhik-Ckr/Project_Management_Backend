ed8467af57987a355e1b6b1ef96a02e1f82204e3
uskdatta <uppalapati.datta@bounteous.com>
2025-06-30 10:34:38 +0530

    Updated Resource.java for lombok purposes

--- src/main/java/com/pm/Project_Management_Server/entity/Resource.java
+++ src/main/java/com/pm/Project_Management_Server/entity/Resource.java
@@ -1,12 +1,16 @@
 package com.pm.Project_Management_Server.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Resource {
 
     @Id
@@ -28,9 +32,12 @@ public class Resource {
 
     private LocalDate endDate;
 
-    private int exp;
 
-    private boolean isAllocated;
+// migrated for lombok issues
+//    private int exp;
+    private Integer exp;
+//    private boolean isAllocated;
+    private boolean allocated;
 
 
     }
